8dfd4701ef6c59c691757e007ba3a2d21a1b7572
aleyssongarcia <aleyssongarcia@gmail.com>
2023-03-16 22:09:52 -0300

    primeiro commit

---
+++ 4-atividade.py
@@ -0,0 +1,6 @@
+# Crie um programa em Python que imprima um convite para um festa com o nome do usuario que for digitado.
+# O nome do usuario precisará ser apresentado todo em caixa alta.
+
+nome = input(" Digite seu nome: ")
+letra_maiuscula = nome.upper()
+print(f'{letra_maiuscula}, Você foi convidado para a festa do Amazing')
