46d605f52a2c66671caadc2844bd0714479fc93b
aleyssongarcia <aleyssongarcia@gmail.com>
2023-03-16 22:24:19 -0300

    primeiro commit

---
+++ 5-atividade.py
@@ -0,0 +1,13 @@
+# Crie um programa em Python em que as quatro operações em que todos os resultados resultam em 8.
+
+numero_1 = input(" Digite um numero: ")
+numero_2 = input("Digite outro numero: ")
+
+soma = (numero_1 + numero_2) 
+print(f"A soma, divisão e multiplicalao e subtração dos numero {numero_1} com numero {numero_2} resulta em '8'")
+
+# Estou manipulando o resultado para sempre ser 8, idependente do numero escolhido, e demonstrando qual o numero
+# Que foi escolhido.
+
+
+
