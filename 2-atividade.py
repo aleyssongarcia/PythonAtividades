e23d05973e5fa549e83403694a264f40c7b86d75
aleyssongarcia <aleyssongarcia@gmail.com>
2023-03-16 21:39:03 -0300

    Primeiro commit

---
+++ 2-atividade.py
@@ -0,0 +1,5 @@
+# Crie um programa em Python que pergunte o nome do usuario e imprima um bom dia com o nome do usuario.
+# Dica: você pode utilizar o metodo.format() ou uma concatenação de string, por exemplo.
+
+nome = input("Qual o seu nome?")
+print(f"{nome}, Bom dia.")
