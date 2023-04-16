ec94e10beaf98198c75f43a0e5f63f52dd637676
aleyssongarcia <aleyssongarcia@gmail.com>
2023-04-16 17:41:38 -0300

    Atividades

---
+++ Lista- Estrutura-Sequencial/1-atividade.py
@@ -0,0 +1,6 @@
+#2 - Faça um programa que converta metros para centimetros.
+
+metros = float(input("Informe um numero em metros: "))
+centimetros = metros * 100
+
+print(f'{centimetros} cm' )
---
+++ atividades/12-atividade.py
@@ -0,0 +1,8 @@
+"""
+1 - Escreva um algoritimo para imprimir os numeros de 1 (inclusive) a 10 (inclusive) em ordem crescente.
+"""
+
+x = 1
+while x < 11:
+    print(x)
+    x += 1
